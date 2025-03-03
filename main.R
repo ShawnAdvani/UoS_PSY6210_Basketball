7059835d6d4bb53ff78c7fb8f2fd11f69600f971
Shawn Advani <54037408+ShawnAdvani@users.noreply.github.com>
2025-03-03 22:50:29 +0000

    Add logistic regression code for team points and wins

---
+++ main.R
@@ -0,0 +1,36 @@
+# Load Data
+data = read.delim("data/Basketball.txt", header = TRUE, sep = "\t", dec = ".")
+
+## Logistic Regression of Basketball Team Points to Wins
+# Build the logit glm
+point_win = glm(Win ~ TeamPoints, data = data, family = binomial (link = 'logit'))
+summary(point_win)
+
+# Calculate LogOdds, Odds, and Probability Space arrays
+point_win_logodds = point_win$coefficients[1]+point_win$coefficients[2]*data$TeamPoints
+point_win_odds = exp(point_win_logodds)
+point_win_probs = point_win_odds/(1+point_win_odds)
+
+# Graph arrays
+plot(data$TeamPoints, point_win_logodds, type = 'p', col = 'red', 
+     main='Team Points Win Ratio: LogOdds', xlab='Team Points', ylab='LogOdds')
+plot(data$TeamPoints, point_win_odds, type = 'p', col = 'green', 
+     main='Team Points Win Ratio: Odds', xlab='Team Points', ylab='Odds')
+plot(data$TeamPoints, point_win_probs, type = 'p', col = 'blue', 
+     main='Team Points Win Ratio: Probabilities', xlab='Team Points', ylab='Probabilities')
+
+## Team Points to Wins Regression Assessment and Analysis
+# Deviance
+point_win_deviance = with(point_win, pchisq(null.deviance - deviance, df.null - df.residual, lower.tail = FALSE))
+
+# Hit Table
+point_win_hit_table = table(data$Win, point_win$fitted.values > 0.5)
+point_win_hit_table
+sum(diag(point_win_hit_table)) / sum(point_win_hit_table)
+
+# LogOdds Probability Ratio
+plot(point_win_probs, point_win_logodds, type = 'p', col = 'purple',
+     main = 'Team Points Win Ratio: LogOdds Probability', xlab = 'Probabilities', ylab = 'LogOdds')
+
+# TODO change all plots to ggplot
+# TODO add analysis summary
