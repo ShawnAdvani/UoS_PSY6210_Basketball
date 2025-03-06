ab77db6c55df68e9d70c1dc1f48796270eaa9c99
Shawn Advani <54037408+ShawnAdvani@users.noreply.github.com>
2025-03-06 23:01:58 +0000

    add notes for summary and add outlier analysis section

--- main.R
+++ main.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(glue)
 
 ##Reading and preparing data -----------------------------------------------------------------------------------------------------------------------
 # Load Data
@@ -9,6 +10,8 @@ data = read.delim("data/Basketball.txt", header = TRUE, sep = "\t", dec = ".")
 # Build the logit glm object
 point_win = glm(Win ~ TeamPoints, data = data, family = binomial (link = 'logit'))
 summary(point_win)
+# Coefficients are statistically significant (p<0.001)
+# AIC = 174.87
 
 # Calculate LogOdds, Odds, and Probability Space arrays
 point_win_logodds = point_win$coefficients[1]+point_win$coefficients[2]*data$TeamPoints
@@ -27,26 +30,39 @@ ggplot(mapping = aes(x = data$TeamPoints, y = point_win_odds)) +
 ggplot(mapping = aes(x = data$TeamPoints, y = point_win_probs)) +
   geom_point(color = "blue", size = 3) +
   labs(title = "Team Points Win Ratio: Probabilities", x = "Team Points", y = "Probabilities")
+# Data shows an upper outlier that could be significant, this exists in all plots and could effect model
 
 ## Team Points to Wins Regression Assessment and Analysis
 # Deviance
 point_win_deviance = with(point_win, pchisq(null.deviance - deviance, df.null - df.residual, lower.tail = FALSE))
+point_win_deviance
+# Low deviance scores 1.8e-09 could indicate a potentially well fit model
 
 # Hit Table
 point_win_hit_table = table(data$Win, point_win$fitted.values > 0.5)
 point_win_hit_table
 sum(diag(point_win_hit_table)) / sum(point_win_hit_table)
+# Hit table shows a 67% success rate indicating medium accuracy
 
 # LogOdds Probability Ratio
 ggplot(mapping = aes(x = point_win_probs, y = point_win_logodds)) +
   geom_point(color = "purple", size = 3) +
   labs(title = "Team Points Win Ratio: LogOdds Probability", x = "Probabilities", y = "LogOdds")
+# Chart shows that there is an issue with the model, as it goes well above logodds 3. Most likely due to upper outlier
 
-
-# TODO add analysis summary
-
-# TODO Note for team members. Fouls and Assists might be good to check next. We can later combine whatever's significant into a single regression analysis or even the predictive model below (did it for fun, might delete).  
-
+# Significant Outliers
+point_win_Q1 = quantile(data$TeamPoints, .25)
+point_win_Q3 = quantile(data$TeamPoints, .75)
+point_win_IQR = IQR(data$TeamPoints)
+outliers = subset(data, data$TeamPoints<(point_win_Q1 - 1.5 * point_win_IQR) | 
+                    data$TeamPoints >(point_win_Q3 + 1.5 * point_win_IQR))
+print(glue('Outliers: {nrow(outliers)}'))
+if (nrow(outliers)>0) {
+  for (i in rownames(outliers)) {
+    print(glue('Outlier Value: {outliers[i, "TeamPoints"]}'))
+  }
+}
+# There is a single significant outlier in the data. Since there is only one, it can be removed and model rerun to determine statistical significance
 
 ##Three point shots to wins -----------------------------------------------------------------------------------------------------------------------
 ## Logistic Regression of Basketball threepoint shots to Wins
@@ -129,18 +145,19 @@ ggplot(mapping = aes(x = assist_win_probs, y = assist_win_logodds)) +
 ##Predictive Model --------------------------------------------------------------------------------------------------------------------------------
 ## For fun turn it into a predictive model
 # split into train and test data
-sample <- sample(c(TRUE, FALSE), nrow(data), replace=TRUE, prob=c(0.7,0.3))
-train <- data[sample, ]
-test <- data[!sample, ] 
+sample = sample(c(TRUE, FALSE), nrow(data), replace=TRUE, prob=c(0.8,0.2))
+train = data[sample, ]
+test = data[!sample, ] 
 
 # create model
-model = glm(Win ~ TeamPoints, data = train, family = binomial (link = 'logit'))
+model = glm(Win ~ TeamPoints + X3PointShots + Assists, data = train, family = binomial (link = 'logit'))
 # test model fit
-pscl::pR2(model)["McFadden"]  # Scores consistently under 0.40, not a good model fit. Still Continuing
+pscl::pR2(model)["McFadden"]  # Scores consistently under 0.40, not a good model fit.
 
-# TODO the following tests multicolinearity, can check if we add other independent varaiables to model
+# TODO the following tests multicolinearity, can check if we add other independent variables to model
 # car::vif(model)
 
 # make predictions
-predicted <- predict(model, test, type="response")
+predicted = predict(model, test, type="response")
 predicted
+
